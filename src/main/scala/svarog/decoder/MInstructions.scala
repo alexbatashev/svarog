dae23b27266054c6e4b40bad199d6098f4be4ca7
Alexander Batashev <alex@batashev.me>
2025-12-31 14:54:41 +0300

    apply fixes

--- src/main/scala/svarog/decoder/MInstructions.scala
+++ src/main/scala/svarog/decoder/MInstructions.scala
@@ -24,39 +24,39 @@ case class MInstructions(xlen: Int) extends Module {
   val funct3 = io.instruction(14, 12)
   val funct7 = io.instruction(31, 25)
 
-  when(opcode === Opcodes.MULDIV && funct7 === 1.U) {
+  when(opcode === Opcodes.MULDIV && funct7 === "b1".U) {
     io.decoded.rd := rd
     io.decoded.rs1 := rs1
     io.decoded.rs2 := rs2
     when(funct3(2) === 0.U) { // Multiply
       io.decoded.opType := OpType.MUL
       switch(funct3(1, 0)) {
-        is("0b00".U) {
+        is("b00".U) {
           io.decoded.mulOp := MulOp.MUL
         }
-        is("0b01".U) {
+        is("b01".U) {
           io.decoded.mulOp := MulOp.MULH
         }
-        is("0b10".U) {
+        is("b10".U) {
           io.decoded.mulOp := MulOp.MULHSU
         }
-        is("0b11".U) {
+        is("b11".U) {
           io.decoded.mulOp := MulOp.MULHU
         }
       }
     }.otherwise { // Divide
       io.decoded.opType := OpType.DIV
       switch(funct3(1, 0)) {
-        is("0b00".U) {
+        is("b00".U) {
           io.decoded.divOp := DivOp.DIV
         }
-        is("0b01".U) {
+        is("b01".U) {
           io.decoded.divOp := DivOp.DIVU
         }
-        is("0b10".U) {
+        is("b10".U) {
           io.decoded.divOp := DivOp.REM
         }
-        is("0b11".U) {
+        is("b11".U) {
           io.decoded.divOp := DivOp.REMU
         }
       }
